11ce6b90dcfe27ea2e77dbe48304ab477fae1afa
AtharvSutar05 <atharv.sutar2209@gmail.com>
2026-06-21 12:04:01 +0530

    console log removed

--- backend/src/middleware/validate_schema.middleware.ts
+++ backend/src/middleware/validate_schema.middleware.ts
@@ -12,7 +12,6 @@ export const validateSchema =
             req.body = schema.parse(req.body);
             next();
         } catch (error) {
-            console.log(error);
             return res.status(400).json({
                 success: false,
                 message: "Validation failed",
